bea98b9eb74b660b34aa0401255864c8ee85d3c5
Fireluci <120563797+Fireluci@users.noreply.github.com>
2026-08-18 14:10:30 +0530

    Update environment variables and logging configuration

--- config.py
+++ config.py
@@ -1,23 +1,39 @@
 import os
 import logging
 
-BOTTOKEN = os.getenv("BOTTOKEN", "")
-MONGODB = os.getenv("MONGODB", "")
+BOTTOKEN = os.getenv("BOTTOKEN", "8191103559:AAHL8HM-MgOPT-AakHL_37mtLaor0XEElDk")
+MONGODB = os.getenv("MONGODB", "mongodb+srv://herostore:herostore@herostore.ywtvule.mongodb.net/?appName=herostore")
 DBNAME = os.getenv("DBNAME", "manager")
+
 APP_ID = int(os.getenv("APP_ID", "1736204"))
-API_HASH = os.getenv("API_HASH", "")
-WORKERS = int(os.getenv("WORKERS", "4"))
+API_HASH = os.getenv("API_HASH", "890d40e0f91a4de32dec2965444b2cbe")
+
 OWNER = int(os.getenv("OWNER", "1058015838"))
 FILES = int(os.getenv("FILES", "-1001205507869"))
 FORCESUB = int(os.getenv("FORCESUB", "0"))
+
 SHORTSITE = os.getenv("SHORTSITE", "nowshort.com")
-SHORTAPI = os.getenv("SHORTAPI", "")
-ADMINS = [int(x) for x in os.getenv("ADMINS", "5051689666 1058015838 6012123382").split() if x.lstrip("-").isdigit()]
+SHORTAPI = os.getenv("SHORTAPI", "c576b9cdd34ceb572a8df1f57eabf7c11efbda3a")
+
+ADMINS = [5051689666, 1058015838, 6012123382]
+
+WORKERS = int(os.getenv("WORKERS", "4"))
 PORT = int(os.getenv("PORT", "8080"))
 
-START_MESSAGE = "<b>🌟 Hello {first},\n\nI'm A File Store Bot 🤖 designed to provide files 📁 and links 🔗</b>"
-FORCE_SUB_MESSAGE = "<b>🌟 Hello {first},\n\nI'm A File Store Bot 🤖 designed to provide files 📁 and links 🔗</b>\n\n<b>You need to join my channel to use this bot.</b>"
+START_MESSAGE = """<b>🌟 Hello {user},
 
-logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(message)s")
+I'm A File Store Bot 🤖 designed to provide files 📁 and links 🔗</b>"""
+
+FORCE_SUB_MESSAGE = """<b>🌟 Hello {user},
+
+You need to join our channel to use this bot.</b>"""
+
+ADMIN_USERNAME = "@MrKrazyBot"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s: %(message)s"
+)
+
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
